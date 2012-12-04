eaeacf8c664e00f950d5b7d078d6c5479ae16353
姚立 <yaoli111144@gmail.com>
2012-12-04 16:37:20 +0800

    Update design_pattern/object/index.js

--- design_pattern/object/index.js
+++ design_pattern/object/index.js
@@ -76,3 +76,105 @@ Kid.prototype.age = 21;
 
 // static varible
 Kid.gender = 'male';
+
+
+/*
+
+/********************************************
+ * For BAT
+ * Author: LeeY<yaoli111144@gmail.com>
+ * Update: 2012-12-4
+ *********************************************/
+
+// Impl
+$(function () {
+	!function(){
+
+		var yaoli = function() {
+			return  new yaoli.fn._init();
+		};
+
+		yaoli.fn = yaoli.prototype =  {
+			constructor: yaoli,
+
+			_init: function() {
+					this._connectEvent();
+					this._getData();
+					return this;
+			},
+			
+			
+			_connectEvent: function() {
+				$('#J-view-commu').click(function(ev) {
+					window.open('detail.htm?recordid='+window.location.href.split('?')[1].split('=')[1], "_self");
+				});
+			},
+			
+			_showTable: function(data) {
+				var cells = '',
+				sizeData = [],
+				scoreData = [],
+				goodnessData = [],
+				mindData = [];
+				for(var i in data) {
+					sizeData.push({id:data[i]['COMMUNITYID'], num:data[i]['COMMUNITYSIZE']});
+					scoreData.push({id:data[i]['COMMUNITYID'], num:data[i]['COMMUNITYSCORE']});
+					goodnessData.push({id:data[i]['COMMUNITYID'], num:data[i]['COMMUNITYGOODNESS']});
+					mindData.push({id:data[i]['COMMUNITYID'], num:Number(data[i]['COMMUNITYLIKEMINDEDNESS'])});	
+				
+					cells += ('<tr> \
+							<td>'+data[i]['COMMUNITYID']+'</td> \
+							<td>'+data[i]['COMMUNITYSIZE']+'</td>\
+							<td>'+data[i]['COMMUNITYSCORE']+'</td>\
+							<td>'+data[i]['COMMUNITYGOODNESS']+'</td>\
+							<td>'+Number(data[i]['COMMUNITYLIKEMINDEDNESS']).toFixed(10)+'</td>\
+							</tr>');
+				}
+				var sizeChart = $('#J-size').chart({'points': sizeData});
+				var scoreChart = $('#J-score').chart({'points': scoreData, 'titleY':'Score'});
+				var goodnessChart = $('#J-goodness').chart({'points': goodnessData, 'titleY':'Goodness'});
+				var mindedChart = $('#J-minded').chart({'points': mindData, 'minTick':0.2, 'titleY':'Like-mindedness'});
+
+				this._renderTable(cells);
+				$('.table-con').mCustomScrollbar();
+
+				$('.table-con table tr').click(function(){
+					$('.table-con table tr').removeClass('active');
+					$(this).addClass('active');
+					var uuid = $(this).children('td:first').text().replace(/ /g, '');
+					
+					sizeChart.highLight(uuid);
+					scoreChart.highLight(uuid);
+					goodnessChart.highLight(uuid);
+					mindedChart.highLight(uuid);
+				});
+			},
+			_renderTable: function(cells) {
+				//console.log(this);
+				$('.table-con table').append(cells);
+			},
+			
+			_getData: function() {
+				
+				try{
+					var _recordId = window.location.href.split('?')[1].split('=')[1];
+					model.getCommunities(_recordId, this._showTable.bind(this));
+				} catch(e) {
+					console.log(e);
+				}	
+			}
+		};
+
+		yaoli.fn._init.prototype = yaoli.fn;
+		window.yaoli = yaoli();
+
+	}();
+});
+
+//$(function () {
+//	console.log(yaoli);
+//});
+
+*/
+
+
